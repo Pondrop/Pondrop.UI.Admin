05a5cca23561db9001e3cd04a1267b8cf8d77c72
Katreena Gabrielle Juntado <katjun@xam.com.au>
2022-08-19 13:13:50 +0800

    Add states to products search field and filteritem

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FunctionComponent, KeyboardEvent, useState } from 'react';
+import { ChangeEvent, FunctionComponent, KeyboardEvent, useState, useEffect } from 'react';
 
 import { InputAdornment } from '@mui/material';
 import { Search } from '@mui/icons-material';
@@ -6,7 +6,9 @@ import { GridColDef, GridFilterModel } from '@mui/x-data-grid';
 
 import { productColumns } from 'components/Grid/constants';
 import { useAppDispatch, useAppSelector } from 'store';
+import { IFilterItem } from 'store/api/types';
 import { useGetProductsQuery } from 'store/api/products/api';
+import { initialState } from 'store/api/products/initialState';
 import { selectProducts, setProductsFilter, setProductsSearchValue } from 'store/api/products/slice';
 import { ColAlignDiv, RowAlignDiv, ContentWrapper, StyledTextField, StyledTitle } from '../styles';
 import Grid from 'components/Grid';
@@ -19,6 +21,16 @@ const Products: FunctionComponent = (): JSX.Element => {
 
   // States
   const [searchValueString, setSearchValueString] = useState<string>('');
+  const [productsFilterItem, setProductsFilterItem] = useState<IFilterItem>(initialState.filterItem);
+
+  // Use Effects
+  useEffect(() => {
+    setProductsFilterItem(filterItem);
+  }, [filterItem]);
+
+  useEffect(() => {
+    setSearchValueString(searchValue ?? '');
+  }, [searchValue]);
 
   // Handlers
   const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
@@ -46,8 +58,8 @@ const Products: FunctionComponent = (): JSX.Element => {
     if (!event?.target?.labels) return;
 
     const combinedValue =
-      filterItem.columnField === currentColumn.field && Array.isArray(filterItem.value)
-        ? handleFilterStateChange(event?.target?.labels[0].outerText, filterItem)
+      productsFilterItem.columnField === currentColumn.field && Array.isArray(productsFilterItem.value)
+        ? handleFilterStateChange(event?.target?.labels[0].outerText, productsFilterItem)
         : [event?.target?.labels[0].outerText];
 
     dispatch(
@@ -90,7 +102,7 @@ const Products: FunctionComponent = (): JSX.Element => {
         id="view-products-grid"
         isFetching={isFetching}
         onFilterModelChange={onFilterModelChange}
-        filterItem={filterItem}
+        filterItem={productsFilterItem}
         handleOnFilterClick={handleOnFilterClick}
       />
     </ContentWrapper>
