5e15e7f9a08cf576021cf9b0094fb73064120e3a
Katreena Gabrielle Juntado <katjun@xam.com.au>
2022-08-24 09:26:04 +0800

    Use react-window to optimize Filter Menu

--- src/components/GridMenu/styles.ts
+++ src/components/GridMenu/styles.ts
@@ -1,43 +1,57 @@
 import styled from "@emotion/styled";
+import { Checkbox } from "@mui/material";
 import { GridColumnMenuContainer } from "@mui/x-data-grid";
+import { FixedSizeList } from "react-window";
 
-export const MenuWrapper = styled(GridColumnMenuContainer)`
+export const MenuWrapper = styled(GridColumnMenuContainer)<{ items: number }>`
+  border: 1px solid rgba(114, 120, 126, 0.5);
   border-radius: 12px;
+  box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.1);
   width: 247px;
+  height: ${({ items = 0 }) => items * 42}px;
   max-height: 290px;
   padding: 10px 0;
-  overflow-y: auto;
-  overflow-x: hidden;
+`;
 
-  & .MuiFormControlLabel-root {
-    display: block;
-    white-space: nowrap;
-    overflow-x: hidden;
-    text-overflow: ellipsis;
-    width: 210px;
-    padding: 0 16px;
-    margin: 0;
+export const RowDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 200px;
 
-    & svg {
-      fill: rgba(28, 27, 31, 0.5);
-    }
-
-    & span {
-      font-size: 14px;
-    }
-
-    & .MuiButtonBase-root {
-      padding: 9px 11px 9px 0px;
-    }
-
-    &:hover {
-      background-color: rgba(0, 0, 0, 0.05);
-      
-      & span {
-        font-weight: 600;
-      }
-    }
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.05);
+    font-weight: 600;
   }
+
+  ::-webkit-scrollbar {
+    width: 5px;
+    background-color: transparent;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    height: 5px;
+    border-radius: 20px;
+    background-color: rgba(0, 0, 0, 0.2);
+  }
+`;
+
+export const LabelDiv = styled.div`
+  line-height: 42px;
+  font-size: 14px;
+  white-space: nowrap;
+  overflow-x: hidden;
+  text-overflow: ellipsis;
+  width: 180px;
+  margin-right: 16px;
+`;
+
+export const StyledCheckbox = styled(Checkbox)`
+  margin-left: 4px;
+  color: rgba(28, 27, 31, 0.5);
+`;
+
+export const StyledList = styled(FixedSizeList)`
+  max-height: 290px;
 
   ::-webkit-scrollbar {
     width: 5px;
