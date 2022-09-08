2dd34f23a4e6983ac9f58798120444015b66fbb8
Katreena Gabrielle Juntado <katjun@xam.com.au>
2022-09-08 12:55:00 +0800

    Add loader while creating category

--- src/pages/CategoryDetails/index.tsx
+++ src/pages/CategoryDetails/index.tsx
@@ -29,7 +29,7 @@ const CategoryDetails: FunctionComponent = (): JSX.Element => {
   const [currentTab, setCurrentTab] = useState<number>(0);
 
   // Microservice endpoints
-  const [createCategory, { isSuccess }] = useCreateCategoryMutation({
+  const [createCategory, { isLoading: isCreateLoading, isSuccess }] = useCreateCategoryMutation({
     fixedCacheKey: 'shared-snackbar-state',
   });
 
@@ -58,20 +58,6 @@ const CategoryDetails: FunctionComponent = (): JSX.Element => {
   const handleAddCategory = () => {
     createCategory(categoryField);
   };
-
-  // useEffects
-  useEffect(() => {
-    if (isSuccess) navigate('../categories', { replace: true });
-  }, [isSuccess]);
-
-  useEffect(() => {
-    dispatch(
-      setCategoryFields({
-        categoryName: '',
-        description: '',
-      }),
-    );
-  }, []);
 
   const renderLoader = () => (
     <CircularLoaderWrapper height="calc(100vh - 36px)">
@@ -165,7 +151,21 @@ const CategoryDetails: FunctionComponent = (): JSX.Element => {
     </div>
   );
 
-  return <ContentDetails>{isLoading ? renderLoader() : renderContent()}</ContentDetails>;
+  // useEffects
+  useEffect(() => {
+    if (isSuccess) navigate('../categories', { replace: true });
+  }, [isSuccess]);
+
+  useEffect(() => {
+    dispatch(
+      setCategoryFields({
+        categoryName: '',
+        description: '',
+      }),
+    );
+  }, []);
+
+  return <ContentDetails>{isLoading || isCreateLoading ? renderLoader() : renderContent()}</ContentDetails>;
 };
 
 export default CategoryDetails;
