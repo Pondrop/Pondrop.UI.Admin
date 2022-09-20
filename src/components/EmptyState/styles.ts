29abae48c79970d69b5b65d81045952835887254
Katreena Gabrielle Juntado <katjun@xam.com.au>
2022-09-20 16:27:12 +0800

    Improve empty state shown when no rows are shown after filter

---
+++ src/components/EmptyState/styles.ts
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+export const EmptyStateWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  svg {
+    fill: rgba(0, 0, 0, 0.5);
+  }
+
+  span {
+    color: rgba(0, 0, 0, 0.5);
+    line-height: 32px;
+  }
+`;
