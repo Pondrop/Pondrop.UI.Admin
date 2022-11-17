b3e4a8debf05c884af579a7478fd592193952625
Katreena Gabrielle Juntado <katjun@xam.com.au>
2022-11-17 11:58:56 +0800

    Fix task details bugs

--- src/pages/SubmissionDetails/components/SubmissionInfo/constants.ts
+++ src/pages/SubmissionDetails/components/SubmissionInfo/constants.ts
@@ -10,3 +10,10 @@ export const submissionTitles = [
     value: 'Redfern'
   },
 ];
+
+export const CATEGORY_PRODUCT_ID = "68a1ddc7-4d18-4cad-9fa9-23fde3dea96c";
+
+export const tooltipContent = {
+  "manual": "Not supplied means the field was optional within the task and the shopper chose not to provide the information. Manual means the product or barcode was not found within the data and was manually entered by the shopper.",
+  "not-supplied": "Not supplied means the field was optional within the task and the shopper chose not to provide the information."
+};
